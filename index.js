8db6b187823f5a4a6b9e5e499097edda13efc1d0
GAD KAMAU <gadkamau46@gmail.com>
2023-07-26 21:52:51 +0300

    Added the game function and updated the other functions

--- index.js
+++ index.js
@@ -1,34 +1,57 @@
-
-function getComputerChoice() {
-    const number = Math.floor(Math.random() * 3);
-        if (number % 3 === 0) {
-                return 'rock';
+function computerPlay(compChoice){
+        let choiceNum = Math.floor(Math.random() * 3);
+        if (choiceNum == 0){
+        compChoice.Value = "rock";
         }
-        if (number % 3 === 1) {
-                return 'paper';
+        else if (choiceNum == 1){
+        compChoice.Value = "paper";
         }
-        return 'scissors';
+        else if(choiceNum == 2){
+        compChoice.Value = "scissors";
+        }
+        console.log(choiceNum);
+        return choiceNum;
 }
-console.log(getComputerChoice())
-
-function playRound ( playerSelection, computerSelection){
-        let win_array = [
-                [0, 2, 1], 
-                [1, 0, 2], 
-                [2, 1, 0]
-        ];
-        let result = win_array[playerSelection][compChoiceInt];
+        
+        function playRound(playerChoiceInt, compChoiceInt, compChoice, playerChoice){
+        /* 0 == rock
+        1 == paper
+        2 == scissor
+        */
+        let win_array = [[0, 2, 1], 
+                        [1, 0, 2], 
+                        [2, 1, 0]];
+        let result = win_array[playerChoiceInt][compChoiceInt];
         if (result == 0){
-        console.log(`Its a tie! You chose ${playerSelection} and The computer chose ${compChoice.Value}`);
+        console.log(`Its a tie! You chose ${playerChoice} and The computer chose ${compChoice.Value}`);
         }
         else if (result == 1){
-        console.log(`You won! You chose ${playerSelection} and The computer chose ${compChoice.Value}`);
+        console.log(`You won! You chose ${playerChoice} and The computer chose ${compChoice.Value}`);
         }
         else if (result == 2){
-        console.log(`You lost! You chose ${playerSelection} and The computer chose ${compChoice.Value}`);
+        console.log(`You lost! You chose ${playerChoice} and The computer chose ${compChoice.Value}`);
         }
 }
-
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+        
+        function game(){
+        let compChoice = {Value: ""};
+        let compChoiceInt;
+        let playerChoiceInt;
+        let playerChoice;
+        
+        for (let i = 0; i < 5; i++){
+        playerChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
+        if (playerChoice == "rock"){
+                playerChoiceInt = 0;
+        }
+        else if (playerChoice == "paper"){
+                playerChoiceInt = 1;
+        }
+        else if (playerChoice == "scissors")
+        {
+                playerChoiceInt = 2;
+        }
+        compChoiceInt = computerPlay(compChoice);
+        playRound(playerChoiceInt, compChoiceInt, compChoice, playerChoice);
+        }       
+}
